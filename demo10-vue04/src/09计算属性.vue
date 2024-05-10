5a0abbde59f0dd0b828a96c416d34a848de0e7b3
sssv587 <1092663295@qq.com>
2024-05-11 00:12:36 +0800

    Vue3-计算属性

---
+++ demo10-vue04/src/09计算属性.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { reactive, computed } from "vue";
+
+/*
+ 计算属性
+*/
+const author = reactive({
+    name: "张三",
+    books: ["java从入门到精通", "算法", "Mysql"]
+})
+// 通过方法返回数据 每使用一次 执行一次
+function hasBooks() {
+    console.log("hasbooks")
+    return author.books.length > 0 ? "是" : "否"
+}
+// 通过计算属性获得数据 每次使用时,如果和上次使用时,数据没有变化,则直接使用上一次的结果
+let bookMessage = computed(() => {
+    console.log("bookMessage")
+    return author.books.length > 0 ? "是" : "否"
+})
+</script>
+
+<template>
+    <div>
+        <p>作者:{{ author.name }}</p>
+        <!-- 会调用5次 -->
+        是否出版过图书:{{ hasBooks() }} <br>
+        是否出版过图书:{{ hasBooks() }} <br>
+        是否出版过图书:{{ hasBooks() }} <br>
+        是否出版过图书:{{ hasBooks() }} <br>
+        是否出版过图书:{{ hasBooks() }} <br>
+        <!-- 只会调用一次,适用于缓存复杂计算的结果 -->
+        是否出版过图书:{{ bookMessage }} <br>
+        是否出版过图书:{{ bookMessage }} <br>
+        是否出版过图书:{{ bookMessage }} <br>
+        是否出版过图书:{{ bookMessage }} <br>
+        是否出版过图书:{{ bookMessage }} <br>
+        
+        是否出版过图书:{{ author.books.length > 0 ? "是" : "否" }} <br>
+    </div>
+</template>
+
+<style scoped></style>
--- demo10-vue04/src/App.vue
+++ demo10-vue04/src/App.vue
@@ -1,60 +1,9 @@
 <script setup>
-import { ref, reactive } from "vue";
 
-/*
-单项绑定 v-bind  响应式数据发生变化时,更新dom树 用户的操作如果造成页面内容的改变不会影响响应式数据
-双向绑定 v-model 页面上的数据由于用户的操作造成了改变,也会同步修改对应的响应式数据
-        双向绑定一般都用于表单标签
-        双向绑定也有人称呼为收集表单信息的命令
-        v-model:value="数据"  双向 绑定
-        v-model:value 一般都省略 :value
-*/
-let message = ref("zhangsan")
-
-let user = reactive({
-    username: "",
-    userPwd: "",
-    intro: "",
-    pro: ""
-})
-let hbs = ref([])
-
-function clearForm() {
-    user.username = ""
-    user.userPwd = ""
-    user.intro = ""
-    user.pro = ""
-    hbs.value.splice(0, hbs.value.length)
-}
 </script>
 
 <template>
-    <div>
-        <!-- <input type="text" v-bind:value="message">
-        <input type="text" v-model="message">
-        {{ message }} -->
-
-        <input type="text" v-model="user.username"> <br>
-        <input type="password" v-model="user.userPwd"> <br>
-        爱好:
-        唱 <input type="checkbox" v-model="hbs" value="sing">
-        跳 <input type="checkbox" v-model="hbs" value="dance">
-        rap <input type="checkbox" v-model="hbs" value="rap">
-        <br>
-        简介:<textarea v-model="user.intro"></textarea>
-        <br>
-        籍贯:
-        <select v-model="user.pro">
-            <option value="1">京</option>
-            <option value="2">津</option>
-            <option value="3">冀</option>
-        </select>
-        <br>
-        <button @click="clearForm()">清空</button>
-        <br>
-        {{ user }} <br>
-        {{ hbs }}
-    </div>
+    
 </template>
 
 <style scoped></style>
