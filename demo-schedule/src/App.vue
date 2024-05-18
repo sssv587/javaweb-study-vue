010b2a608f011aa18f9a59e51c4be1c04a88d00d
sssv587 <1092663295@qq.com>
2024-05-18 17:42:42 +0800

    Vue3-日志管理-重构前端工程

---
+++ demo-schedule/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+    <div>
+        <Header></Header>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+
+<style scoped></style>
