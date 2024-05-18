728811a32eadbf0c2415c80b9074172699db42e5
sssv587 <1092663295@qq.com>
2024-05-18 16:51:07 +0800

    Vue3-路由守卫

--- demo14-router/src/routers/router.js
+++ demo14-router/src/routers/router.js
@@ -43,4 +43,31 @@ const router = createRouter({
     ]
 })
 
+
+// 设置全局前置守卫
+// 每次路由切换页面前,都会执行beforeEach中的回调函数
+router.beforeEach(
+    (to, from, next) => {
+        /*
+            from 上一个页面 从哪里来
+            to   下一个页面 到哪里去
+            next 放行的方法 只有执行了该方法 才会放行路由 filterChain.doFilter
+                  next()         放行
+                  next("/路径")  路由的重定向
+        */
+        console.log("beforeEach")
+        console.log(from.path)
+        console.log(to.path)
+        next()
+    }
+)
+
+// 设置全局后置守卫
+// 每次路由切换页面后,都会执行afterEach中的回调函数
+router.afterEach(
+    (to, from) => {
+        console.log("afterEach")
+    }
+)
+
 export default router
---
+++ demo14-router/src/路由传参.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function showDetail(id, language) {
+  // router.push(`/showDetail/${id}/${language}`)
+  router.push({ path: `/showDetail/${id}/${language}` })
+}
+
+function showDetail2(param_id, param_language) {
+  // router.push(`/showDetail2?id=${id}&language=${language}`)
+  router.push({ path: '/showDetail2', query: { id: param_id, language: param_language } })
+}
+</script>
+
+<template>
+  <div>
+    <router-link to="/showDetail/1/java">声明式路由路径传参</router-link> <br>
+    <button @click="showDetail(2, 'PHP')">编程式路由路径传参</button> <br>
+    <hr>
+    <router-link to="/showDetail2?id=1&language=java">声明式路由键值对传参</router-link> <br>
+    <router-link v-bind:to="{ path: '/showDetail2', query: { id: 2, language: 'php' } }">声明式路由键值对传参</router-link> <br>
+    <button @click="showDetail2(3, 'Python')">编程式路由键值对传参</button> <br>
+    <router-view></router-view>
+    <hr>
+  </div>
+</template>
+
+<style scoped></style>
