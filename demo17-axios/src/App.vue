316bfa88037ca9d846ebd1f74aea8d087935fcf9
sssv587 <1092663295@qq.com>
2024-05-24 01:13:01 +0800

    Vue3-Axios-请求和响应拦截器

--- demo17-axios/src/App.vue
+++ demo17-axios/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import axios from 'axios'
+import request from './axios'
 import { reactive, ref } from 'vue';
 
 let message = reactive({
@@ -8,44 +8,8 @@ let message = reactive({
 })
 
 function getLoveWords() {
-  // 发送get请求的方法
-  // axios.get(url)
-  // axios.get(url,{请求的其他信息})
-  // axios.get(url,{params:{键值对参数},headers:{设置一些特殊的请求头}})
-  // return axios.get("https://api.uomg.com/api/rand.qinghua?format=json")
-  // return axios.get(
-  //   "https://api.uomg.com/api/rand.qinghua",
-  //   {
-  //     params: {
-  //       format: "json",
-  //       username: "zhangsan",
-  //       userpwd: "lisi"
-  //     },
-  //     headers: {
-  //       Accept: "application/json, text/plain, text/html/, */*"
-  //     }
-  //   }
-  // )
-
-  // axios.post(url,{要放入请求体中的JSON串},{请求的其他信息})
-  // return axios.post(
-  //   "https://api.uomg.com/api/rand.qinghua",
-  //   {
-  //     username: "zhangsan",
-  //     userpwd: "lisi"
-  //   },
-  //   {
-  //     params: {
-  //       format: "json"
-  //     },
-  //     headers: {
-  //       Accept: "application/json, text/plain, text/html/, */*"
-  //     }
-  //   }
-  // )
-
-  return axios.post(
-    "https://api.uomg.com/api/rand.qinghua?format=json"
+  return request.post(
+    "api/rand.qinghua?format=json"
   )
 }
 
